991c0e93f85b04f1811686d0a9b9f8675fda0b9c
Gali-Sai-Divakar-reddy <galisaidevakerreddy@gmail.com>
2024-05-25 22:41:44 -0400

    added create_tables.py

---
+++ postgres_ETL/create_tables.py
@@ -0,0 +1,21 @@
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+def create_database():
+    """
+    - Creates and connects to the sparkifydb
+    - Returns the connection and cursor to sparkifydb
+    """
+    pass
+
+def drop_tables():
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    """
+    pass
+
+def create_tables():
+    """
+    Creates each table using the queries in `create_table_queries` list. 
+    """
+    pass
